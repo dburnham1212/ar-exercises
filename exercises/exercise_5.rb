d3868f8f348b294f0762156adbd03d8f635c26e2
dburnham1212 <dburnham1212@gmail.com>
2023-07-24 17:24:10 -0400

    Finished Excercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total revenue for all stores: #{Store.sum(:annual_revenue)}"
+puts "Average revenue for all stores: #{Store.average(:annual_revenue)}"
+
+puts "Total stores generating $1M or more #{Store.where(annual_revenue: 1000000..).size}"
